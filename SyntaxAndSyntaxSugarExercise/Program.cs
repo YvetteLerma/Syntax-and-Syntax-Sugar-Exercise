21bcd01e432a0f4c1b0775c977ffdf80d50e38cb
Yvette Lerma <lerma.yvette@hotmail.com>
2022-10-20 13:13:26 -0600

    code has been added

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -1,10 +1,15 @@
-﻿namespace SyntaxAndSyntaxSugarExercise
+﻿using System;
+
+namespace SyntaxAndSyntaxSugarExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var answer = int.Parse(Console.ReadLine());
+            
+            var response = answer < 9 ? $"{answer} is less than 9" : $"{answer} is greater than 9";
+            Console.WriteLine(response);
         }
     }
 }
